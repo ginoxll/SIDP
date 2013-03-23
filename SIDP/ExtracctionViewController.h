d4d4d17485a8b00a070dfe3a5f24b0b5f0118845
Miguel Angel <miguel_seus@hotmail.com>
2013-03-23 14:53:09 -0700

    Avance de recepción

---
+++ SIDP/ExtracctionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExtracctionViewController.h
+//  SIDP
+//
+//  Created by Miguel Angel Vásquez Jiménez on 23/03/13.
+//  Copyright (c) 2013 GinoX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExtracctionViewController : UIViewController
+
+@end
---
+++ SIDP/ExtracctionViewController.m
@@ -0,0 +1,38 @@
+//
+//  ExtracctionViewController.m
+//  SIDP
+//
+//  Created by Miguel Angel Vásquez Jiménez on 23/03/13.
+//  Copyright (c) 2013 GinoX. All rights reserved.
+//
+
+#import "ExtracctionViewController.h"
+
+@interface ExtracctionViewController ()
+
+@end
+
+@implementation ExtracctionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
